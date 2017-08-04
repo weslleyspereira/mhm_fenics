ac1da0c36dbcc7132d57b1da7fb5de765840f152
weslley.spereira <weslley.spereira@gmail.com>
2017-08-03 21:42:36 -0300

    Improvement in geometry classes Mark boundaries function

--- demos/meshes/geometry.py
+++ demos/meshes/geometry.py
@@ -4,7 +4,7 @@ Created on 3 de ago de 2017
 @author: weslley
 '''
 
-# from dolfin.cpp.mesh import Point
+from dolfin.cpp.mesh import Point
 
 class Geometry():
     """Geometry class"""
@@ -20,3 +20,18 @@ class Geometry():
         
     def getNoFaces(self):
         return len(self._faces)
+    
+    def getNormals(self):
+        geoNormals = [Point() for i in range(self._faces)]
+        if self._nDim == 2:
+            for i in range(self.getNoFaces()):
+                v = self._points[self._faces[i][1]]-self._points[self._faces[i][0]]
+                geoNormals[i] = Point(v[1],-v[0])
+        elif self._nDim == 3:
+            geoNormals = [
+                (self._points[face[1]]-self._points[face[0]]).cross(self._points[face[2]]-self._points[face[0]]) for face in self._faces
+            ]
+        else:
+            print("Invalid dimension!")
+            exit(1)
+        return geoNormals
